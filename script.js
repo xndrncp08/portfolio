c4160000a6b97a2f9c27548099a6c4dd822b3332
xder-08 <xander.rancap@edu.sait.ca>
2025-09-15 12:29:47 -0600

    fixed contact button

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ document.getElementById("darkToggle").addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
   });
   
+<button class="contact-btn" onclick="document.getElementById('contact').scrollIntoView({behavior: 'smooth'});">
+  Contact
+</button>
